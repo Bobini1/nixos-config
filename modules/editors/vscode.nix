3b5228671f512ed9cbf093d513440ecfaac77600
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2025-11-26 19:29:48 +0100

    Add some required vscode settings

--- modules/editors/vscode.nix
+++ modules/editors/vscode.nix
@@ -1,27 +1,34 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   programs.vscode = {
     enable = true;
     package = pkgs.vscode-fhs; # For rust-analyzer etc
     profiles.default = {
-      extensions = with pkgs.vscode-extensions; [
-        bbenoist.nix
-        jnoortheen.nix-ide
-        arrterian.nix-env-selector
-        kamadorueda.alejandra
-        ms-pyright.pyright
-        rust-lang.rust-analyzer
-        ms-python.black-formatter
-        mkhl.direnv
-        fill-labs.dependi
-        ms-vscode.cmake-tools
-        ms-vscode.cpptools
-      ] ++ pkgs.nix4vscode.forVscode [
-        "theqtcompany.qt-core"
-        "theqtcompany.qt-qml"
-      ];
+      extensions = with pkgs.vscode-extensions;
+        [
+          bbenoist.nix
+          jnoortheen.nix-ide
+          arrterian.nix-env-selector
+          kamadorueda.alejandra
+          ms-pyright.pyright
+          rust-lang.rust-analyzer
+          ms-python.black-formatter
+          mkhl.direnv
+          fill-labs.dependi
+          ms-vscode.cmake-tools
+          ms-vscode.cpptools
+        ]
+        ++ pkgs.nix4vscode.forVscode [
+          "theqtcompany.qt-core"
+          "theqtcompany.qt-qml"
+        ];
       userSettings = {
         github.copilot.nextEditSuggestions.enabled = true;
+        qt-core.additionalQtPaths = [
+          {
+            name = "Qt-6.10.0-linux-g++_from_PATH";
+            path = "${pkgs.kdePackages.qtbase}/bin/qtpaths";
+          }
+        ];
       };
     };
   };
