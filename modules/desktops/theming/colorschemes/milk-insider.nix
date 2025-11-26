30da577bcd01b80b09f75617a7b6a6bbcb978fc3
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2025-11-26 03:01:27 +0100

    Custom color scheme
    Milk!

---
+++ modules/desktops/theming/colorschemes/milk-insider.nix
@@ -0,0 +1,24 @@
+{
+  slug = "milk-insider";
+  name = "Milk Insider";
+  variant = "dark";
+  author = "Bobini";
+  palette = {
+    base00 = "#0d0e13"; # ----
+    base01 = "#482939"; # ---
+    base02 = "#612d39"; # --
+    base03 = "#793037"; # -
+    base04 = "#903435"; # +
+    base05 = "#a83733"; # ++
+    base06 = "#c45754"; # +++
+    base07 = "#e07877"; # ++++
+    base08 = "#b12f2d"; # red
+    base09 = "#89544c"; # orange
+    base0A = "#cead9f"; # yellow
+    base0B = "#254525"; # green
+    base0C = "#8ea595"; # aqua
+    base0D = "#616593"; # blue
+    base0E = "#522a42"; # purple
+    base0F = "#301c1e"; # brown
+  };
+}
--- modules/desktops/theming/stylix.nix
+++ modules/desktops/theming/stylix.nix
@@ -1,31 +1,35 @@
-{ pkgs, config, ... }:
-
 {
+  pkgs,
+  config,
+  ...
+}: {
   stylix = {
-    enable = true;autoEnable = true;
+    enable = true;
+    autoEnable = true;
     #
     image = ../wallpapers/milk1.jpg;
-    #base16Scheme = ./colorschemes/matcha-dark.yaml;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
-#     cursor = {
-#       size = 16;
-#       package = pkgs.phinger-cursors;
-#       name = "phinger-cursors-light";
-#     };
+    base16Scheme = import ./colorschemes/milk-insider.nix;
+    #    base16Scheme = "${pkgs.base16-schemes}/share/themes/caroline.yaml";
+    #     cursor = {
+    #       size = 16;
+    #       package = pkgs.phinger-cursors;
+    #       name = "phinger-cursors-light";
+    #     };
+    #    polarity = "dark";
     fonts = {
       monospace = {
         package = pkgs.nerd-fonts.jetbrains-mono;
         name = "JetBrains Mono Nerd Font";
       };
-#       serif = config.stylix.fonts.monospace;
-#       sansSerif = config.stylix.fonts.monospace;
-#       emoji = config.stylix.fonts.monospace;
-#       sizes = {
-#         desktop = 12;
-#         terminal = 12;
-#         popups = 12;
-#         applications = 12;
-#       };
+      #       serif = config.stylix.fonts.monospace;
+      #       sansSerif = config.stylix.fonts.monospace;
+      #       emoji = config.stylix.fonts.monospace;
+      #       sizes = {
+      #         desktop = 12;
+      #         terminal = 12;
+      #         popups = 12;
+      #         applications = 12;
+      #       };
     };
   };
 }
